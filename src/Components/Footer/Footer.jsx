21c233a64ba2357ba4b0aa7816d05042b686b017
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-10-24 23:42:48 +0600

    Footer done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import  './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer'>
+      <div className="top">
+        <div className="item">
+          <h1>Category</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoe</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officiis architecto facilis quod optio, necessitatibus omnis, cupiditate eius officia doloremque obcaecati nostrum repudiandae 
+            
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officiis architecto facilis quod optio, necessitatibus omnis, cupiditate eius officia doloremque obcaecati nostrum repudiandae 
+            
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className='logo'>HT</span>
+          <span className='copyright'> @ Copyright 2023. All Rights Reserved</span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
