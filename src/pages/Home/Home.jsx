331850ee8488cc86040de587d6a0257a12aa8b64
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-11-05 22:42:57 +0600

    Categories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Home.scss';
 import Slider from '../../Components/Slider/Slider'
 import FeaturedProducts from '../../Components/FeaturedProducts/FeaturedProducts';
+import Categories from '../../Components/Categories/Categories';
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider />
       <FeaturedProducts type= "featured"/>
+      <Categories />
       <FeaturedProducts type= "trending"/>
     </div>
   )
